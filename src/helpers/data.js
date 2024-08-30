422e4729bd43f58fbd61fef28ca2b7e99b869f4d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-30 13:33:30 +0300

    file created

---
+++ src/helpers/data.js
@@ -0,0 +1,113 @@
+export const data = [
+  {
+    name: "Efes Antik Kenti",
+    history: [
+      "Efes Antik Kenti, M.Ö. 10. yüzyıldan itibaren tarih sahnesinde önemli bir rol oynadı.",
+      "İzmir iline bağlı Selçuk ilçesinde konumlanmıştır.",
+      "Artemis Tapınağı ve Celsus Kütüphanesi gibi önemli yapıları içerir.",
+      "Efes, Roma İmparatorluğu'nun Asya eyaletinin başkenti olmuştur.",
+    ],
+    img: "https://cdn.pixabay.com/photo/2014/09/05/16/07/ephesos-436437_1280.jpg",
+  },
+
+  {
+    name: "Aphrodisias Antik Kenti",
+    history: [
+      "Aphrodisias Antik Kenti, M.Ö. 5. yüzyıldan itibaren sanat ve kültür merkezi olarak parladı.",
+      "Aydın iline bağlı Karacasu ilçesinde konumlanmıştır.",
+      "Adını antik çağın aşk ve güzellik tanrıçası Afrodit'ten almıştır.",
+      "Antik tiyatrosu, stadyumu ve tapınaklarıyla ünlüdür.",
+    ],
+    img: "https://www.aphrodisias.org/wp-content/uploads/2014/12/aphrodisias-614x511.jpg",
+  },
+  {
+    name: "Perge Antik Kenti",
+    history: [
+      "Perge Antik Kenti, M.Ö. 1000 yılından beri zengin bir tarih ve kültüre ev sahipliği yapmaktadır.",
+      "Antalya ili, Aksu ilçesinde yer almaktadır.",
+      "Helenistik, Roma ve Bizans dönemlerine ait kalıntıları barındırır.",
+      "Perge, Likya-Pamphylia bölgesinin önemli bir antik şehriydi.",
+    ],
+    img: "https://cdn.seturday.com/content/uploads/makale_resimleri/kapak/perge-antik-kentini-kesfedelim-cc80809b-d6cc-4dad-b6ed-f3236e968647banner-min.jpg",
+  },
+  {
+    name: "Hierapolis Antik Kenti",
+    history: [
+      "Hierapolis Antik Kenti, M.Ö. 2. yüzyılda Roma döneminde popüler bir termal merkezdi.",
+      "Denizli ili, Pamukkale ilçesinde yer almaktadır.",
+      "Hierapolis, termal sularıyla ünlü olup ziyaretçilere şifa sağlayan bir yerdi.",
+      "UNESCO Dünya Mirası Listesi'nde yer alan antik kent, doğal ve tarihi zenginliklere ev sahipliği yapmaktadır.",
+    ],
+    img: "https://1.bp.blogspot.com/--r0ycUWkFkA/XrvzIzjqyJI/AAAAAAACP10/Zlv9C1wZ7k0TNMhVzWE99s96ZyAl84VVACLcBGAsYHQ/s1600/asl%25C4%25B1-bora%2Bdenizli%2B-kleopatra%2Bhavuz%2B-hierapolis.jpeg",
+  },
+  {
+    name: "Assos Antik Kenti",
+    history: [
+      "Assos Antik Kenti, M.Ö. 900 yılına kadar uzanan zengin bir antik geçmişe sahiptir.",
+      "Çanakkale ili, Ayvacık ilçesinde yer almaktadır.",
+      "Athena Tapınağı ve antik tiyatrosuyla ön plana çıkar.",
+      "Antik dönemde önemli bir Ege liman kenti olarak ticaret merkeziydi.",
+    ],
+    img: "https://www.ayryapi.com.tr/wp-content/uploads/2018/03/Ayryapi_Assos-Antik-Kenti.jpg",
+  },
+  {
+    name: "Knidos Antik Kenti",
+    history: [
+      "Knidos Antik Kenti, M.Ö. 4. yüzyılda önemli bir ticaret ve kültür merkezi olarak parlamıştır.",
+      "Muğla ili, Datça ilçesinde yer almaktadır.",
+      "Antik tiyatrosu, agora ve Afrodit Tapınağı gibi önemli yapıları içerir.",
+      "Knidos, antik dönemde ticaret yolu üzerinde stratejik bir konumda bulunmaktaydı.",
+    ],
+    img: "https://cdnuploads.aa.com.tr/uploads/Contents/2020/09/06/thumbs_b_c_5c547da0819c6b8e1d37594f10e83e10.jpg?v=122701",
+  },
+  {
+    name: "Xanthos Antik Kenti",
+    history: [
+      "Xanthos Antik Kenti, M.Ö. 8. yüzyılda Likya uygarlığına ev sahipliği yapmıştır.",
+      "Antalya ili, Kaş ilçesinde yer almaktadır.",
+      "Pers İmparatorluğu tarafından ele geçirilen kent, direniş ve fedakarlıkla tanınır.",
+      "Xanthos, UNESCO Dünya Mirası Listesi'nde yer alan antik kentler arasındadır.",
+    ],
+    img: "https://blog.otelz.com/wp-content/uploads/2023/04/xanthos-antik-kenti.jpg",
+  },
+  {
+    name: "Sardis Antik Kenti",
+    history: [
+      "Sardis Antik Kenti, M.Ö. 1200 yılından itibaren Lidya Krallığı'nın başkenti oldu.",
+      "Manisa ili, Salihli ilçesinde yer almaktadır.",
+      "Krallık döneminde zenginliği ve ihtişamıyla bilinir.",
+      "Antik tiyatro, Artemis Tapınağı ve kral mezarları kentin önemli noktalarındandır.",
+    ],
+    img: "https://gezigezgini.com/wp-content/uploads/2022/12/sardes-antik-kenti.jpg",
+  },
+  {
+    name: "Myra Antik Kenti",
+    history: [
+      "Myra Antik Kenti, M.Ö. 5. yüzyılda Likya Birliği'nin önemli bir üyesiydi.",
+      "Antalya ili, Demre ilçesinde yer almaktadır.",
+      "Aziz Nikolaos'un doğduğu yer olarak bilinen Myra, Likya döneminden kalma tiyatrosu ile ünlüdür.",
+      "Kaya mezarları ve antik kalıntılarıyla ziyaretçilere tarihi bir atmosfer sunmaktadır.",
+    ],
+    img: "https://www.kulturportali.gov.tr/contents/images/20190730120027239_myra%20444%20(1).jpg",
+  },
+  {
+    name: "Sagalassos Antik Kenti",
+    history: [
+      "Sagalassos Antik Kenti, M.Ö. 333 yılında Büyük İskender tarafından fethedildi.",
+      "Burdur ili, Ağlasun ilçesinde yer almaktadır.",
+      "Dağlık bir bölgede kurulan Sagalassos, antik dönemin önemli kültür ve sanat merkezlerinden biriydi.",
+      "Antoninler Çeşmesi ve antik tiyatrosu, kentin en bilinen yapıları arasındadır.",
+    ],
+    img: "https://belediyehaber.net/wp-content/uploads/2023/02/kapak-2.jpg",
+  },
+  {
+    name: "Side Antik Kenti",
+    history: [
+      "Side Antik Kenti, M.Ö. 7. yüzyılda kurulan antik bir liman şehri olarak tarih sahnesinde yer aldı.",
+      "Antalya ili, Manavgat ilçesinde yer almaktadır.",
+      "Antik tiyatro, Apollon Tapınağı ve agora gibi önemli yapıları içermektedir.",
+      "Antik kentin sahil şeridi ve antik kalıntıları, ziyaretçilere eşsiz bir atmosfer sunar.",
+    ],
+    img: "https://im.haberturk.com/2021/05/04/3060786_03a146ff0f4ca532de1dd873eed28dc1_640x640.jpg",
+  },
+];
